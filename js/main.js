74bb1e23c3642d4193a478f217c9a096cbb8d008
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-05-11 11:44:58 +0500

    while

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// JavaScript da If else ----------------------------------
+
+// let age = 16;
+// let userAge = prompt("yoshingizni kiriting...");
+
+// if (userAge < age){
+//     console.log(`Sizning yoshingiz ${userAge} yoshda. Passport olish yoshi ${age} dan boshlab`);
+// }
+// else if(userAge > age){
+//     console.log(`Sizning yoshinigiz ${age} dan katta va siz Passport olishingiz mumkin`);
+// }
+// else{
+//     console.log(`Sizning yoshingiz belgilangan ${age} yoshda va Siz Passport olishingiz mumkin`);
+// }
+
+// while loop
+
+let i = 1
+
+while (i <= 10){
+    a = i++
+    console.log(a);
+}
